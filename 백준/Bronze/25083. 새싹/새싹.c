7190ea18d3c476acb00bb2e64abb7c9e49d3a33f
이인석 <95563956+PinkTultle@users.noreply.github.com>
2024-01-26 15:37:38 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+int main() {
+
+	puts("         ,r\'\"7");
+	puts("r`-_   ,'  ,/");
+	puts(" \\. \". L_r\'");
+	puts("   `~\\/");
+	puts("      |");
+	puts("      |");
+
+	return 0;
+}
