0d954c086bfde352ce3a5da6b14e3e447afd5320
이인석 <95563956+PinkTultle@users.noreply.github.com>
2024-02-20 20:39:40 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1116 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.c
+++ 백준/Bronze/15829. Hashing/Hashing.c
@@ -3,31 +3,30 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int hash_var(int count) {
-	int R = 31;
+unsigned long long hash_var(int count) {
+	unsigned long long R = 31;
 	if (count == 0) return 1;
-	return R * hash_var(count - 1);
+	return (R * hash_var(count - 1)) % 1234567891;
 }
 
 int main() {
 
 	int len;
 	char* str;
-	int point, result = 0;
+	unsigned long long point, result = 0;
 
 	scanf("%d", &len);
 
 	str = (char*)calloc(len + 1, sizeof(char));
 	scanf("%s", str);
 
-
 	for (int i = 0; i < len; i++) {
 		
 		point = *(str + i);
 		point -= 96;
 		
-
 		result += point * hash_var(i);
+		result %= 1234567891;
 	}
 
 	printf("%d\n", result%1234567891);
