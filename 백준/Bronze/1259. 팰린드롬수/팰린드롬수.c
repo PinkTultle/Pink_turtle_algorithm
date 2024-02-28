378d5902c85ffef04dd024a7ceaeb5aa2e57c938
이인석 <95563956+PinkTultle@users.noreply.github.com>
2024-02-28 18:32:58 +0900

    [Bronze I] Title: 팰린드롬수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.c
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.c
@@ -1,75 +1,23 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<stdlib.h>
-
-int Digit(int input) {
-	int count = 0;
-
-	while (input >= 10) {
-		input /= 10;
-		count++;
-	}
-	return count + 1;
-}
-
-int main() {
-
-	int input;
-
-	while(1){
-
-		scanf("%d", &input);
-
-		if (input == 0) return 0;
-
-		int digits, Criteria, status = 1;
-
-		digits = Digit(input);
-		
-		switch (digits) {
-
-		case 1 :
-			puts("yes");
-			continue;
-
-		case 3:
-			Criteria = 1;
-			break;
-
-		case 5:
-			Criteria = 2;
-			break;
-
-		default :
-			Criteria = digits / 2;
-			break;
-		}
-
-		int* temp = (int*)calloc(Criteria, sizeof(int));
-		status = 1;
-
-		for (int i = 0; i < Criteria; i++) {
-			*(temp + i) = input % 10;
-			input /= 10;
-		}
-
-		if (digits == 3 || digits == 5) {
-			input /= 10;
-		}
-
-		for (int i = --Criteria; i >= 0; i--) {
-			if (*(temp + i) != input % 10) 
-				status = 0;
-			input /= 10;
-		}
-
-		if (status) {
-			puts("yes");
-		}
-		else {
-			puts("no");
-		}
-	}
-
-	return 0;
+#include<string.h>
+int main(){
+    int i, j, check;
+    char value[100000];
+    while(scanf("%s", value) && value[0] != '0'){
+        check = 1;
+        j = strlen(value);
+        
+        for(i=0;i<j/2;i++){
+            if(value[i] != value[j-1-i]){
+                check = 0;
+                break;
+            }
+        }
+        if(check == 1){
+            printf("yes\n");
+        }
+        else if (check == 0){
+            printf("no\n");
+        }
+    }
 }
