4797ad4894d07453700190204b7e171b03f9d124
이인석 <95563956+PinkTultle@users.noreply.github.com>
2024-10-07 00:13:33 +0900

    [Bronze II] Title: 분해합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,44 @@
+//
+//	CLASS : 2
+//	백준  문제 2231
+//	문제 명 : 분해합
+//
+
+#include <iostream>
+
+int main(){
+	int input = 0;
+	int compare_num = 0;
+	int tmp = 0;
+
+	std::cin >> input;
+
+	for(tmp = input; tmp > 10;){
+		tmp /= 10;
+		compare_num += 9;
+	}
+
+	compare_num += tmp;
+	compare_num = input - compare_num;
+
+	for(tmp = compare_num; compare_num < input; tmp = compare_num){
+		int creat_num = compare_num;
+		
+		while (tmp > 0) {
+			creat_num += (tmp % 10);
+			tmp /= 10;
+		}
+		// creat_num += tmp;
+		
+		if (creat_num == input) {
+			std::cout << compare_num << std::endl;
+			return 0;
+		}
+		else {
+			compare_num++;
+		}
+	}
+
+	std::cout << '0' << std::endl;
+	return 0;
+}
